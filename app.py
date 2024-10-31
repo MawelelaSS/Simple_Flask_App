1ba114cbbe5cb532c5a6830dce9c7261b3f38c7d
Sibusiso <sibusisosicelomawelela@gmail.com>
2024-10-31 19:34:14 +0200

    2nd commit

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods = ['POST', 'GET'])
 def login():
     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
     return redirect(url_for("home"))
+
+@app.route("/UserDashboard" , methods= ['POST' , 'GET'])
+def dashboard():
+    return render_template("UserDashboard.html")    
 
 # when submit is clicked
 # GET    is not secured it shows the passowrd in the URL
@@ -25,6 +29,11 @@ def submit():
     else: 
         user = request.args.get('username')
         return f"logged in successfully by get, hello {user}"
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")        
 
 if __name__ == "__main__":
     app.run(debug=True) 
